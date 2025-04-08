24e5cb9d8528c1dbdef5cee04519cc4ccace851b
EduMnzs <eduardo.santosm1202@gmail.com>
2025-04-08 17:41:39 -0300

    Endpoints configured correctly

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const itemRoutes = require('./routes/itemRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');
 const sequelize = require('./config/database');
 const Item = require('./models/item');
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use('/api/itens', itemRoutes);
+app.use('/api', uploadRoutes);
 
 sequelize.sync();
 
--- server.js
+++ server.js
@@ -1,11 +1,5 @@
 require('dotenv').config();
-const express = require('express');
-const uploadRoutes = require('./routes/uploadRoutes');
-
-const app = express();
-
-app.use(express.json());
-app.use('/api', uploadRoutes);
+const app = require('./app');
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
